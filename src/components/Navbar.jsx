9cefd7e38b897191ada1176203fcbe03bf5b1953
JoseCarlos <jocdiazmuic@gmail.com>
2021-12-16 21:57:22 -0500

    Footer and navbar components without routing

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/components/Navbar.scss';
+
+const Navbar = () => {
+  return (
+    <nav className='navbar'>
+      <h1>Home</h1>
+      <h1>About</h1>
+      <h1> Cart</h1>
+    </nav>
+  );
+};
+
+export default Navbar;
