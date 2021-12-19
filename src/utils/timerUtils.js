1b0a4258499dffd109ceb4d676fa421f9c65e7fe
José C. Díaz <jocdiazmuic@gmail.com>
2021-12-19 16:24:17 -0500

    Revert "Feature/offer timer"

--- src/utils/timerUtils.js
+++
@@ -1,7 +0,0 @@
-// 0. Funciones de interes
-// eslint-disable-next-line import/prefer-default-export
-const randomTimer = (min = 5, max = 180) => {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-};
-
-module.exports = { randomTimer };
