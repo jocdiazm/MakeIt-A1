3acbd6710e9532860a519f8a65e7b815382cc4be
JoseCarlos <jocdiazmuic@gmail.com>
2021-12-20 10:12:05 -0500

    footer updates

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,14 +6,16 @@ const Footer = () => {
     'https://makeitreal.camp/assets/logo-black-baffa903a5ee8ad3c94f085fd551487207b3eeae296f2331880f8004b2af7743.png';
   return (
     <div className='footer'>
-      <p> Hecho con ❤️ para </p>
-      <img src={imgSrc} alt='Make It Real' />
       <span>
-        {' by '}
+        {' '}
+        Hecho con ❤️ by{' '}
         <a className='hero' href='https://github.com/jocdiazm'>
           @jocdiazm
-        </a>
+        </a>{' '}
+        para{' '}
       </span>
+
+      <img src={imgSrc} alt='Make It Real' />
     </div>
   );
 };
