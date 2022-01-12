184e70ada651afab4220c6d500e72ff1dcc7155f
JoseCarlos <jocdiazmuic@gmail.com>
2022-01-12 17:40:16 -0500

    fixing navbar styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/components/Navbar.scss';
+// get our fontawesome imports
+import { faHome, faUser } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Navbar = () => {
   return (
     <nav className='Navbar'>
-      <Link to='/'>Home</Link>
-      <Link to='/about'>About</Link>
+      <Link className='text-link' to='/'>
+        <FontAwesomeIcon icon={faHome} /> Home
+      </Link>
+      <Link className='text-link' to='/about'>
+        <FontAwesomeIcon icon={faUser} /> About
+      </Link>
     </nav>
   );
 };
